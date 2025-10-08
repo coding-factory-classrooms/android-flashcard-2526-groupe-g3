5d617abb2111b738f8f3e0ef79fb8cf5c894b6ab
Axel <axel.letourneur-chiffoleau@edu.esiee-it.fr>
2025-10-08 11:05:28 +0200

    Feature / Create the json system to read the questions

---
+++ app/src/main/java/com/example/flashcard/Question.java
@@ -0,0 +1,9 @@
+package com.example.flashcard;
+
+import java.util.List;
+
+public class Question {
+    public String string;
+    public List<String> Image;
+    public int correct;
+}
---
+++ app/src/main/java/com/example/flashcard/QuestionData.java
@@ -0,0 +1,7 @@
+package com.example.flashcard;
+
+import java.util.List;
+
+public class QuestionData {
+    public List<Difficulty> difficulties;
+}
